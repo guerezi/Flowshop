ae377966071da5e89edb48da5716e10030f624b9
guerezi <gabrielguerezi@hotmail.com>
2022-04-05 22:54:01 -0300

    i do not agree with this lint

--- main.py
+++ main.py
@@ -7,22 +7,23 @@ import numpy
 
 # SEED = 873654221
 
+
 def makespan(instancia, solucao):
     nM = len(instancia)
     tempo = [0] * nM
 
     tarefa = [0] * len(solucao)
-    
+
     for t in solucao:
         if tarefa[t-1] == 1:
             return "SOLUÇÃO INVÁLIDA: tarefa repetida!"
         else:
             tarefa[t-1] = 1
-        
-        for m in range (nM):
-            if tempo[m] < tempo[m-1] and m!=0:
-                tempo[m] = tempo[m-1] 
-                
+
+        for m in range(nM):
+            if tempo[m] < tempo[m-1] and m != 0:
+                tempo[m] = tempo[m-1]
+
             tempo[m] += instancia[m][t-1]
 
     return tempo[nM-1]
@@ -33,19 +34,22 @@ def lerInstancias(listaArquivos):
     for file in listaArquivos:
         f = open(file, "r")
         job = f.read()
-        job = job[job.index("processing times :")+19 : job.index("\nnumber of jobs")].split("\n")
-        job = list(map( lambda list: [ int(i) for i in re.split(r"\s+", list.strip()) ], job))
-    
+        job = job[job.index("processing times :") +
+                  19: job.index("\nnumber of jobs")].split("\n")
+        job = list(map(lambda list: [int(i)
+                   for i in re.split(r"\s+", list.strip())], job))
+
         jobs.append(job)
     return jobs
+
 
 def criarPopulacaoInicial(instancia, tamanho):
     permutation = []
 
     nlist = list(range(1, len(instancia)))
     for _ in range(tamanho):
-        random.shuffle( nlist )
-        permutation.append( nlist.copy() )
+        random.shuffle(nlist)
+        permutation.append(nlist.copy())
 
     return permutation
 
@@ -54,54 +58,61 @@ def avaliarPop(populacao):
     result = []
     for solucao in populacao:
         result.append(makespan(instancia, solucao))
-    
+
     return result
+
 
 def retornaMelhorSolucao(populacao, aptidaoPop):
     return {
         'solucao': populacao[aptidaoPop.index(min(aptidaoPop))],
-        'aptidao': min(aptidaoPop), 
+        'aptidao': min(aptidaoPop),
         'tempoFinal': 0
-    }    
+    }
+
 
 def selecionarPop(populacao, aptidaoPop):
     conj = list(zip(populacao, aptidaoPop))
-    conj.sort(key= lambda t: t[1] )
-    return [ list(i)[0] for i in conj[0:(len(populacao) * 0.51).__floor__() or 1] ]
+    conj.sort(key=lambda t: t[1])
+    return [list(i)[0] for i in conj[0:(len(populacao) * 0.51).__floor__() or 1]]
+
 
 def recombinacao(populacaoSelecionada):
     for pop in populacaoSelecionada:
-        inicio = random.randint(0,len(pop))
+        inicio = random.randint(0, len(pop))
         fim = inicio + (len(pop) * 0.33).__floor__()
 
         if (inicio > fim):
             inicio, fim = fim, inicio
 
-        pop[inicio:fim] = numpy.roll(pop[inicio:fim],random.randint(1,3))
+        pop[inicio:fim] = numpy.roll(pop[inicio:fim], random.randint(1, 3))
 
     return populacaoSelecionada
+
 
 def mutacao(novasSolucoes):
     for array in novasSolucoes:
         while True:
             if (random.random() < 0.05):
-                pos = random.randint(1,len(array) -1)
-                array[pos], array[ len(array) - pos] = array[len(array) - pos], array[pos]
+                pos = random.randint(1, len(array) - 1)
+                array[pos], array[len(
+                    array) - pos] = array[len(array) - pos], array[pos]
             else:
-                break            
+                break
 
     return novasSolucoes
+
 
 def selecionarNovaGeracao(populacaoAtual, novasSolucoes):
     newGen = (populacaoAtual + novasSolucoes)
     random.shuffle(newGen)
-    return newGen[0: ((len(newGen) * 0.5).__floor__() or 1)] 
+    return newGen[0: ((len(newGen) * 0.5).__floor__() or 1)]
+
 
 def salvarRelatorio(relatorios):
     f = open('result.json', 'a+')
 
     for relatorio in relatorios:
-        apt, tempo, solucoes = [],[],[]
+        apt, tempo, solucoes = [], [], []
 
         for i in range(len(relatorio)):
             apt.append(relatorio[i]['aptidao'])
@@ -112,9 +123,9 @@ def salvarRelatorio(relatorios):
         try:
             data = {
                 "job": files[relatorios.index(relatorio)],
-                "order": str(sorted(list(zip(apt, solucoes)), key= lambda t: t[0])[0][1]),
-                "lower": min(apt), 
-                "upper": max(apt), 
+                "order": str(sorted(list(zip(apt, solucoes)), key=lambda t: t[0])[0][1]),
+                "lower": min(apt),
+                "upper": max(apt),
                 "mdApt": median(apt),
                 "desApt": stdev(apt),
                 "mdTime": median(tempo),
@@ -125,7 +136,8 @@ def salvarRelatorio(relatorios):
         except:
             print(relatorio)
 
-files = list(numpy.repeat( [
+
+files = list(numpy.repeat([
     "flowshop/tai20_5.txt",
     "flowshop/tai20_10.txt",
     "flowshop/tai20_20.txt",
@@ -146,20 +158,23 @@ for instancia in listaInstancias:
     tamanhoPop = len(instancia) * 2
     tempoMaximo = 10
 
-    for it in range (10):
-        melhorSolucao = {'solucao':[], 'aptidao': 9223372036854775807 , 'tempoFinal':0}
+    for it in range(10):
+        melhorSolucao = {'solucao': [],
+                         'aptidao': 9223372036854775807,
+                         'tempoFinal': 0,
+                         }
         tempoInicial = time.time()
         populacao = criarPopulacaoInicial(instancia, tamanhoPop)
-        
+
         i = 0
         while True:
             if tempoMaximo <= time.time() - tempoInicial:
                 break
-            
+
             aptidaoPop = avaliarPop(populacao)
             melhorSolucaoAtual = retornaMelhorSolucao(populacao, aptidaoPop)
             i -=- 1
-            
+
             if melhorSolucao['aptidao'] > melhorSolucaoAtual['aptidao']:
                 i = 0
                 melhorSolucao = melhorSolucaoAtual
@@ -168,12 +183,12 @@ for instancia in listaInstancias:
             novasSolucoes = recombinacao(populacaoSelecionada)
             novasSolucoes = mutacao(novasSolucoes)
             populacao = selecionarNovaGeracao(populacao, novasSolucoes)
-            
+
             if i > 100:
                 break
 
         melhorSolucao['tempoFinal'] = time.time() - tempoInicial
         melhoresSolucoes[it] = melhorSolucao
-    relatorio[ relatorio.index({}) ] = melhoresSolucoes
+    relatorio[relatorio.index({})] = melhoresSolucoes
 
 salvarRelatorio(relatorio)
